2f559adebdc2bc8160e4f63344fc3fae4b527ea4
Sagar todakar <todakar.sagar@gmail.com>
2023-03-14 04:12:20 +0530

    new project

--- src/main/java/com/baseclasses/utilites/dataexcelfile.java
+++ src/main/java/com/baseclasses/utilites/dataexcelfile.java
@@ -1,6 +1,40 @@
 package com.baseclasses.utilites;
 
-public class dataexcelfile {
+import com.baseclasses.baseuse;
+import org.apache.poi.ss.usermodel.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class dataexcelfile extends baseuse {
+    public static JSONArray mapdatafromexcel() throws IOException {
 
 
+        // Read the Excel file
+        File file = new File("src/main/resources/excelfile.xlsx");
+        Workbook workbook = WorkbookFactory.create(file);
+        Sheet sheet = workbook.getSheetAt(0);
+        JSONArray jsonArray = new JSONArray();
+        Row cloum = sheet.getRow(0);
+        int colnum = cloum.getLastCellNum();
+        for (int j = 0; j < sheet.getRow(0).getLastCellNum() + 1; j++) {
+            JSONObject rowObject = new JSONObject();
+            for (int i = 0; i < colnum; i++) {
+                rowObject.put(sheet.getRow(0).getCell(i).getStringCellValue(), sheet.getRow(j + 1).getCell(i).getStringCellValue());
+            }
+            jsonArray.add(rowObject);
+        }
+
+        String jsonString = jsonArray.toString();
+      //  System.out.println(jsonString);
+        return jsonArray;
+    }
 }
+
+
+
+
+
--- src/test/java/step/definitions/LoginStepDef.java
+++ src/test/java/step/definitions/LoginStepDef.java
@@ -2,7 +2,9 @@ package step.definitions;
 
 import com.baseclasses.baseuse;
 import com.baseclasses.modules.loginpage;
+import com.baseclasses.utilites.dataexcelfile;
 import com.baseclasses.utilites.propertiesutils;
+import com.google.gson.JsonArray;
 import io.cucumber.java.bs.A;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
@@ -14,21 +16,29 @@ import static org.junit.Assert.*;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.json.simple.JSONArray;
 import org.junit.Assert;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 import java.util.PropertyPermission;
 
 public class LoginStepDef extends baseuse {
     Properties pro= propertiesutils.loadalltheproperties();
     Response response;
+    HashMap<String,String[]> values =new HashMap<String,String[]>();
     @Given("Navigate to the login page")
-    public void navigateToTheLoginPage() throws InterruptedException {
+    public void navigateToTheLoginPage() throws InterruptedException, IOException {
         String endpoint=pro.getProperty("endpoint").toString();
         RestAssured.baseURI=endpoint;
+
   // String username=pro.getProperty("env.username").toString();
     // loginpage.setusername(username);
 //assertEquals(true,false);
+        JSONArray values= dataexcelfile.mapdatafromexcel();
+        System.out.println(values);
     }
 
     @When("user enter the username and password")
